c6f7e542525f5c26df91c43704fbaefcc2c3c2b6
AshleyMSM1 <84349146+AshleyMSM1@users.noreply.github.com>
2021-06-11 21:46:36 -0400

    work in progress

---
+++ scratchpad/scratchpad.js
@@ -0,0 +1,14 @@
+let movie = {
+    name: "It",
+    runTime: 90,
+    genre: "Comedy",
+    characters: ["Pennywise", "Thor"],
+    personOne: [
+      {name: "Pennywise", age: 3, items: ["Knife", "Binky", "Diaper"]} ],
+    personTwo: [
+        {name: "Thor", age: 1000000, items: ["Fireballs", "Cheetah", "Cheetos"]} ,]
+    };
+
+    console.log(movie);
+    console.log(movie.personOne);
+    console.log(movie.personTwo);
